6a736e8848023e618a8413ed2baeea8134bd1ad8
MuriloCastanheira <murilobicho@gmail.com>
2022-05-20 11:24:17 -0300

    first commit

--- VSSS_Codigos/posiciona.py
+++ VSSS_Codigos/posiciona.py
@@ -6,6 +6,7 @@ import FIRALib
 
 def posicionaBAIXA(id,team,x,y):
     contador = 1
+    
     while contador==1:
         
         if( team == True):
@@ -14,37 +15,47 @@ def posicionaBAIXA(id,team,x,y):
             PosicaoCarro = FIRALib.AzulCar(id)
 
         
-        if (y) > (PosicaoCarro[1]+0.61):## Se a bola estiver a direita do carrinho
-            C = ((x) - (-PosicaoCarro[0]+0.72))
-            B = ((y) - (PosicaoCarro[1]+0.61))
-            if (x) < (-PosicaoCarro[0]+0.72):
+        pc1 = PosicaoCarro[1] + 0.61
+        pc2 =-PosicaoCarro[0] + 0.72
+        pc3 = PosicaoCarro[2] + 1.55
+        pc4 = PosicaoCarro[2] - 1.55
+        pc5 = PosicaoCarro[2] + 0.299
+        pc6 = PosicaoCarro[2] - 0.299
+        pc7 = PosicaoCarro[2] + 0.298
+        pc8 = PosicaoCarro[2] - 0.298
+        pc9 = PosicaoCarro[2] + 0.398
+        pc10 = PosicaoCarro[2] - 0.398
+        if (y) > (pc1):## Se a bola estiver a direita do carrinho
+            C = ((x) - (pc2))
+            B = ((y) - (pc1))
+            if (x) < (pc2):
                 theta =  -(3.1 + math.atan(B/C))
                 
             else:
                 theta =  -math.atan(B/C)
 
-        if (y) < (PosicaoCarro[1]+0.61):## Se a bola estiver a direita do Esquerda
-            C = (-(x) + (-PosicaoCarro[0]+0.72))
-            B = ((y) - (PosicaoCarro[1]+0.61))
-            if (x) < (-PosicaoCarro[0]+0.72):
+        if (y) < (pc1):## Se a bola estiver a direita do Esquerda
+            C = (-(x) + (pc2))
+            B = ((y) - (pc1))
+            if (x) < (pc2):
                 theta =  ( math.atan(B/C) + 3.1)
             else:
                 theta =  math.atan(B/C)
 ##################################################################################################################################################################
 
-        print("THETA: ",theta,"CARRINHO ORIENTAÇÂO: ",PosicaoCarro[2])   
+        #print("THETA: ",theta,"CARRINHO ORIENTAÇÂO: ",PosicaoCarro[2])   
 
-        if( PosicaoCarro[2] + 1.55 > theta and PosicaoCarro[2] - 1.55 < theta ):
-            if ((PosicaoCarro[2]+0.299) > theta):
+        if( pc3 > theta and pc4 < theta ):
+            if ((pc5) > theta):
         
                 FIRALib.movimente(0,team,-20,-50)#direita
 
 
-            if ((PosicaoCarro[2]- 0.299) < theta):
+            if ((pc6) < theta):
 
                 FIRALib.movimente(0,team,-50,-20)#esquerda
 
-            if((PosicaoCarro[2]+0.398) > theta and theta > (PosicaoCarro[2]- 0.398) ):
+            if((pc9) > theta and theta > (pc10) ):
 
                 FIRALib.movimente(0,team,-30,-30)
         else:
@@ -55,33 +66,33 @@ def posicionaBAIXA(id,team,x,y):
 
             else:
                 if( theta > 0):
-                    if ((PosicaoCarro[2]+0.299 + 3) < theta):
+                    if ((pc5 + 3) < theta):
         
                         FIRALib.movimente(0,team,20,50)#Esquerda
 
 
-                    if ((PosicaoCarro[2]- 0.299 + 3) > theta):
+                    if ((pc6 + 3) > theta):
 
                         FIRALib.movimente(0,team,50,20)#Direita
                 
-                    if((PosicaoCarro[2]+0.298 +3) > theta and theta > (PosicaoCarro[2]- 0.298 + 3) ):
+                    if((pc7 +3) > theta and theta > (pc8 + 3) ):
 
                         FIRALib.movimente(0,team,30,30)
 
                 if( theta < 0):
-                    if ((PosicaoCarro[2]+0.299 - 3) < theta):
+                    if ((pc5 - 3) < theta):
         
                         FIRALib.movimente(0,team,20,50)#Esquerda
 
 
-                    if ((PosicaoCarro[2]- 0.299 - 3) > theta):
+                    if ((pc6 - 3) > theta):
 
                         FIRALib.movimente(0,team,50,20)#Direita
                 
-                    if((PosicaoCarro[2]+0.298 -3) > theta and theta > (PosicaoCarro[2]- 0.298 - 3) ):
+                    if((pc7 -3) > theta and theta > (pc8 - 3) ):
 
                         FIRALib.movimente(0,team,30,30)
-        if (-PosicaoCarro[0]+0.72 + 0.05 > x and -PosicaoCarro[0]+0.72 - 0.05 < x) and (PosicaoCarro[1]+0.61 + 0.05 > y and PosicaoCarro[1]+0.61 - 0.05 < y):
+        if (pc2+ 0.05 > x and pc2 - 0.05 < x) and ( pc1 + 0.05 > y and pc1 - 0.05 < y):
             FIRALib.movimente(id,team,0,0) 
             contador = 0
 
@@ -98,19 +109,31 @@ def SeguiBola(id,team):
         if( team == False):
             PosicaoCarro = FIRALib.AzulCar(id)
 
+        pc1 = PosicaoCarro[1] + 0.61
+        pc2 =-PosicaoCarro[0] + 0.72
+        pc3 = PosicaoCarro[2] + 1.55
+        pc4 = PosicaoCarro[2] - 1.55
+        pc5 = PosicaoCarro[2] + 0.299
+        pc6 = PosicaoCarro[2] - 0.299
+        pc7 = PosicaoCarro[2] + 0.298
+        pc8 = PosicaoCarro[2] - 0.298
+        pc9 = PosicaoCarro[2] + 0.398
+        pc10 = PosicaoCarro[2] - 0.398
+        pb1 = PosicaoBola[1] + 0.61
+        pb2 = -PosicaoBola[0] + 0.72
 
-        if (PosicaoBola[1]+0.61) > (PosicaoCarro[1]+0.61):## Se a bola estiver a direita do carrinho
-            C = ((-PosicaoBola[0] + 0.72) - (-PosicaoCarro[0]+0.72))
-            B = ((PosicaoBola[1]+  0.61) - (PosicaoCarro[1]+0.61))
-            if (-PosicaoBola[0] + 0.72) < (-PosicaoCarro[0]+0.72):
+        if (pb1) > (pc1):## Se a bola estiver a direita do carrinho
+            C = ((pb2) - (pc2))
+            B = ((pb1) - (pc1))
+            if (pb2) < (pc2):
                 theta =  -(3.1 + math.atan(B/C))
             else:
                 theta =  -math.atan(B/C)
 
-        if (PosicaoBola[1]+0.61) < (PosicaoCarro[1]+0.61):## Se a bola estiver a direita do Esquerda
-            C = (-(-PosicaoBola[0] + 0.72) + (-PosicaoCarro[0]+0.72))
-            B = ((PosicaoBola[1]+  0.61) - (PosicaoCarro[1]+0.61))
-            if (-PosicaoBola[0] + 0.72) < (-PosicaoCarro[0]+0.72):
+        if (pb1) < (pc1):## Se a bola estiver a direita do Esquerda
+            C = (-(pb2) + (pc2))
+            B = ((pb1) - (pc1))
+            if (pb2) < (pc2):
                 theta =  ( math.atan(B/C) + 3.1)
             else:
                 theta =  math.atan(B/C)
@@ -118,19 +141,19 @@ def SeguiBola(id,team):
 ###########################################################################################################
        # print("THETA: ",theta,"CARRINHO ORIENTAÇÂO: ",PosicaoCarro[2])   
 
-        if( PosicaoCarro[2] + 1.55 > theta and PosicaoCarro[2] - 1.55 < theta ):
-            if ((PosicaoCarro[2]+0.299) > theta):
+        if( pc3 > theta and pc4 < theta ):
+            if ((pc5) > theta):
         
                 FIRALib.movimente(0,team,-20,-50)#direita
 
 
-            if ((PosicaoCarro[2]- 0.299) < theta):
+            if ((pc6) < theta):
 
                 FIRALib.movimente(0,team,-50,-20)#esquerda
 
-            if((PosicaoCarro[2]+0.398) > theta and theta > (PosicaoCarro[2]- 0.398) ):
+            if((pc9) > theta and theta > (pc10) ):
 
-                FIRALib.movimente(0,team,-30,-30)
+                FIRALib.movimente(0,team,-33,-33)
         else:
             if(theta < -2 and PosicaoCarro[2] > 2):
                 FIRALib.movimente(0,team,-50,-20)#esquerda
@@ -139,29 +162,29 @@ def SeguiBola(id,team):
 
             else:
                 if( theta > 0):
-                    if ((PosicaoCarro[2]+0.299 + 3) < theta):
+                    if ((pc5 + 3) < theta):
         
                         FIRALib.movimente(0,team,20,50)#Esquerda
 
 
-                    if ((PosicaoCarro[2]- 0.299 + 3) > theta):
+                    if ((pc6+ 3) > theta):
 
                         FIRALib.movimente(0,team,50,20)#Direita
                 
-                    if((PosicaoCarro[2]+0.298 +3) > theta and theta > (PosicaoCarro[2]- 0.298 + 3) ):
+                    if((pc7 +3) > theta and theta > (pc8 + 3) ):
 
-                        FIRALib.movimente(0,team,30,30)
+                        FIRALib.movimente(0,team,33,33)
 
                 if( theta < 0):
-                    if ((PosicaoCarro[2]+0.299 - 3) < theta):
+                    if ((pc5 - 3) < theta):
         
                         FIRALib.movimente(0,team,20,50)#Esquerda
 
 
-                    if ((PosicaoCarro[2]- 0.299 - 3) > theta):
+                    if ((pc6- 3) > theta):
 
                         FIRALib.movimente(0,team,50,20)#Direita
                 
-                    if((PosicaoCarro[2]+0.298 -3) > theta and theta > (PosicaoCarro[2]- 0.298 - 3) ):
+                    if((pc7 -3) > theta and theta > (pc8- 3) ):
 
-                        FIRALib.movimente(0,team,30,30)
+                        FIRALib.movimente(0,team,33,33)
